90be299a4d0c80f92bc3ffe1c0fa59ac2be70bc7
Kerry <teches12345678@gmail.com>
2025-11-23 16:58:42 +0800

    Finish setting up folders [assets, models, views, auth], resources such as images. Almost done with my login page as well.

---
+++ lib/auth/login.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+
+class _LoginFormState extends State<LoginForm> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  late String errormessage;
+  late bool isError;
+
+  @override
+  void initState(){
+    errormessage = "This is an error message";
+    isError = false;
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+  void checkLogin (username, password){
+    setState((){
+      if (username == ""){
+        errormessage = "Please enter your username!";
+      }
+      else if (password == ""){
+        errormessage = "Please enter your password!";
+        isError = true;
+      }else{
+        errormessage = "";
+        isError = false;
+      }
+      
+      });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/facebook-logo.png',
+                    height: 48,
+                    width: 48,
+                  ),
+                  const SizedBox(width: 12,),
+                  Text("FACEBOOK", style: txtStyle,),
+                ],
+              ),
+              SizedBox(height: 15,),
+              TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.person_rounded),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock_rounded),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              ElevatedButton(
+                style:ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Colors.blue, 
+                ),
+                onPressed: (){
+                  checkLogin(usernameController.text, passwordController.text);
+                  if (!isError){
+                    Navigator.push(
+                      context, MaterialPageRoute(builder: (context)=> SocialMedia()),
+                    );
+                  }
+                },
+                child: Text('LOGIN', style: txtStyle2),
+              ),
+              SizedBox(height: 15,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+var txtStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontSize: 14,
+);
+
+var registerTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontSize: 14,
+);
+
+var errorTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontSize: 14,
+);
+
+var txtStyle2 = const TextStyle(
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontSize: 24,
+  color: Colors.white,
+);
